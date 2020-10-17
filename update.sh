72ace77a23f921a67817d8bde6a4bb848b337050
Dion Christensen <dion@trexug.com>
2020-10-18 00:20:22 +0200

    Fix additional typo

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git pull
-docker build . trexlock
+docker build . -t trexlock
 mkdir -p ~/trexlock/data
 docker container rm -f trexlock
 docker run trexlock -d --name=trexlock -P 8223:443 -v ~/trexlock/data:/data --restart unless-stopped 
