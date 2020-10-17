07a77a73d3f1b64eb1365cfec8a0691ac70bfddf
Dion Christensen <dion@trexug.com>
2020-10-18 00:30:23 +0200

    Update start script

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ git pull
 docker build . -t trexlock
 mkdir -p ~/trexlock/data
 docker container rm -f trexlock
-docker run trexlock -d --name=trexlock -P 8223:443 -v ~/trexlock/data:/data --restart unless-stopped 
+docker run -d --name trexlock --restart unless-stopped trexlock -P 8223:443 -v ~/trexlock/data:/data 
