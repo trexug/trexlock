f2ec0787c9f61afec24c405961a0833237ff85f1
Dion Christensen <dion@trexug.com>
2020-10-19 20:46:32 +0200

    Allow gpio

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ git pull
 docker build . -t trexlock
 mkdir -p ~/trexlock/data
 docker container rm -f trexlock
-docker run -d --name trexlock --restart unless-stopped -p 8223:443 -v ~/trexlock/data:/data trexlock
+docker run --name trexlock --restart unless-stopped --device /dev/gpiomem -p 8223:443 -v ~/trexlock/data:/data -d trexlock
